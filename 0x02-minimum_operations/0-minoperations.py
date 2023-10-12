7f5815b2e69467bc81a368ca6d8633046d79bdd5
ETHreds <jcmaglaw@gmail.com>
2023-10-12 18:55:51 +0300

    Stuuf

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+"""minOperations whatever the function do"""
+
+
+def minOperations(n):
+    """ Whatever the function does"""
+    if n <= 1:
+        return 0
+    
+    def find_divisors(number):
+        divisors = []
+        for divisor in range(2, number + 1):
+            while number % divisor == 0:
+                divisors.append(divisor)
+                number //= divisor
+        return divisors
+    
+    prime_divisors = find_divisors(n)
+    operations = 0
+    clipboard = 0
+    
+    for divisor in prime_divisors:
+        if is_prime(divisor):
+            clipboard += divisor
+        else:
+            operations += (clipboard // divisor)  # Copy-Paste
+            clipboard %= divisor
+    
+    operations += clipboard  # Paste
+    
+    return operations
+
+# Helper function to check if a number is prime
+def is_prime(number):
+    if number <= 1:
+        return False
+    if number <= 3:
+        return True
+    if number % 2 == 0 or number % 3 == 0:
+        return False
+    i = 5
+    while i * i <= number:
+        if number % i == 0 or number % (i + 2) == 0:
+            return False
+        i += 6
+    return True
+
---
+++ README.md
@@ -0,0 +1,1 @@
+Sample INterview questions
